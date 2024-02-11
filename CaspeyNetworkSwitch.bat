97cab77269c11d7b40a97f8d8156cdb9eba6268d
Casper Long <48057288+casper64114@users.noreply.github.com>
2024-02-11 14:58:22 -0500

    Add files via upload

--- CaspeyNetworkSwitch.bat
+++ CaspeyNetworkSwitch.bat
@@ -1,8 +1,8 @@
 @echo off
-cls 
+cls
 
 echo Choose a network adapter:
-echo ========================
+echo ======================== 
 echo 1. Ethernet
 echo 2. Ethernet 2
 echo 3. WiFi
@@ -13,22 +13,24 @@ set /p choice="Enter choice (1-5): "
 
 if %choice%==1 set adapter=Ethernet
 if %choice%==2 set adapter=Ethernet 2
-if %choice%==3 set adapter=WiFi
+if %choice%==3 set adapter=WiFi  
 if %choice%==4 set adapter=WiFi 2
 if %choice%==5 set /p adapter="Enter name or IP: "
 
 echo.
-echo Now choose an application to launch:
-echo ====================================  
+echo Now choose an application to launch: 
+echo ====================================
 set /p apppath="Enter full path to app: "
 
 echo.
 echo Launching %apppath% on %adapter%...
 
-start %apppath% 
+netsh interface ip set address name="%adapter%" source=static addr=192.168.1.101 mask=255.255.255.0 gateway=192.168.1.1 gwmetric=0
+start %apppath%
+netsh interface ip set address "Ethernet" source=dhcp
 
 echo.
 set /p join="Would you like to join my Discord server? (Y/N): "
-if /I "%join%" == "Y" start https://discord.gg/MvR43saFGf
+if /I "%join%" == "Y" start https://discord.gg/MvR43saFGf 
 
 pause
